27a5f88a9589d194e424c9910ed931add1e911bf
ZhaopingJJXie <jojoxie6@gmail.com>
2020-06-12 17:03:53 -0500

    download file from internet

--- code2.R
+++ code2.R
@@ -1,6 +1,10 @@
 library(tidyverse)
 library(dslabs)
-data(heights)
-heights
-female_heights <- heights %>% filter(sex == "Female") %>%.$height
-hist(female_heights)
+library(readr)
+# data(heights)
+# heights
+# female_heights <- heights %>% filter(sex == "Female") %>%.$height
+# hist(female_heights)
+url <- "http://mlr.cs.umass.edu/ml/machine-learning-databases/breast-cancer-wisconsin/wdbc.data"
+dat <- read_csv(url, col_names = FALSE)
+dat
